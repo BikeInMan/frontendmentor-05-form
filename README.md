bdc6b8bff11c9a42f1b802b442ba6c8ff6302a30
bikeinman <praveen.t.o@gmail.com>
2022-02-13 18:53:48 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*
+
+### Screenshot
+
+![](./screenshots/screenshot1.png) ![](./screenshots/screenshot2.png)
+
+
+### Links
+
+- Solution URL: [bikeinman.github.io](https://bikeinman.github.io/frontend-mentor-05-form/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+
+### What I learned
+
+1. How to style forms correctly
+2. How to display custom validation errors
+3. Use of responsive layout techinques
+4. How to validate individually and also again on submit
+
+### Continued development
+
+Must improve JavaScript validation logic to make it more simple and concise.
+
+
+### Useful resources
+
+- [MDN Forms](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/oninvalid) - This helped me on how to start on validations in Javascript.
+- [MDN Forms Guide](https://developer.mozilla.org/en-US/docs/Learn/Forms) - Helped me to understand forms in a much deeper sense.
+
+
+## Author
+
+- Frontend Mentor - [@BikeInMan](https://www.frontendmentor.io/profile/BikeInMan)
+
+## Acknowledgments
+All thanks to frontend mentor for providing such a great challenge.
---
+++ main.js
@@ -0,0 +1,106 @@
+const form = document.getElementById('form');
+
+// error msg elements
+const fnameError = document.getElementById('fname-err');
+const lnameError = document.getElementById('lname-err');
+const emailError = document.getElementById('email-err');
+const passwordError = document.getElementById('password-err');
+
+//input fields
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+let anyErr = false;
+
+function checkFname(){
+    fnameError.setAttribute('hidden',true);
+    fname.classList.remove("onerror");
+    let err = emptyCheck(fname.value)
+    if(err){
+        fnameError.removeAttribute('hidden');
+        fname.classList.add("onerror");
+        anyErr = true;
+    }
+}
+
+function checkLname(){
+    lnameError.setAttribute('hidden',true);
+    lname.classList.remove("onerror");
+
+    let err = emptyCheck(lname.value)
+    if(err){
+        lnameError.removeAttribute('hidden');
+        lname.classList.toggle("onerror");
+        anyErr = true;
+    }
+};
+
+function checkEmail(){
+    emailError.setAttribute('hidden',true);
+    email.classList.remove("onerror");
+
+    let err = validateEmail(email.value);
+    if(err){
+        emailError.removeAttribute('hidden');
+        email.classList.toggle("onerror");
+        anyErr = true;
+    }
+};
+
+function checkPassword(){
+    passwordError.setAttribute('hidden',true);
+    password.classList.remove("onerror");
+
+    let err = emptyCheck(password.value);
+    if(err){
+        passwordError.removeAttribute('hidden');
+        password.classList.toggle("onerror");
+        anyErr = true;
+    }
+};
+
+function emptyCheck(val){
+    let err = true;
+    if(val){
+        let actVal = val.trim();
+        if(actVal.length > 0){
+            err = false;
+        }
+    }
+    return err;
+}
+
+function validateEmail(val){
+
+    let err = true;
+    if(emptyCheck(val)){
+        return err;
+    }
+
+    if(val){
+        const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        err = !emailRegExp.test(val); //returns true on valid email
+    }
+    return err;
+}
+
+// check for errors on blur
+fname.onblur = checkFname;
+lname.onblur = checkLname;
+email.onblur = checkEmail;
+password.onblur = checkPassword;
+
+// check for errors on submit
+form.onsubmit = function(event) {
+    checkFname();
+    checkLname();
+    checkEmail();
+    checkPassword();
+    if(anyErr){
+        // For this example, don't actually submit the form
+        event.preventDefault();
+        // submit();
+    }
+}
